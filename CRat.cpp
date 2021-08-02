a1c9ab775ec76cca6284d9a220217e29497c8d9c
uliana-titanyuk <63346238+uliana-titanyuk@users.noreply.github.com>
2021-08-02 14:22:07 +0300

    Update CRat.cpp

--- CRat.cpp
+++ CRat.cpp
@@ -1,3 +1,4 @@
+
 #include "CRat.h"
 #include<fstream>
 #include <iostream>
@@ -9,10 +10,11 @@ CRat::CRat(const std::vector < std::pair< int, unsigned int > >& values) {
 }
 
 CRat::CRat(const char* FileName) {
-    std::ifstream inf{ FileName };
-    if (!inf) { 
-        throw "File error.null pointer"; 
+    if (FileName) {
+        throw "File error.null pointer";
     }
+    std::ifstream inf{ FileName };
+    
     if (!inf.is_open())
     {
         throw "File can not be opened.";
@@ -46,83 +48,99 @@ CRat::~CRat() {
 };
 
 CRat CRat::operator+(const CRat& b) {
-    int  n;
+    size_t n = std::min(fractions.size(), b.fractions.size());
     CRat v;
-    n = fractions.size();
-    for (int i = 0; i < n; i++) {
-        if ((fractions[i].first != 0) && (b.fractions[i].first != 0)) {
-            v.fractions[i].first = fractions[i].first * b.fractions[i].second + fractions[i].second * b.fractions[i].first;
-            v.fractions[i].second = b.fractions[i].second * fractions[i].second;
-            int a = v.fractions[i].first;
-            int b = v.fractions[i].second;
+    for (size_t i = 0; i < n; i++) {
+        std::pair <int, unsigned int> p=std::make_pair(fractions[i].first * b.fractions[i].second + fractions[i].second * b.fractions[i].first,b.fractions[i].second * fractions[i].second);
+        v.fractions.push_back(p);
+    }
+    if (n < fractions.size())
+    {
+        for (size_t i = 0; i < fractions.size(); i++) {
+            v.fractions.push_back(fractions[i]);
         }
-     }
-   
+    }
+        if (n < fractions.size())
+        {
+            for (size_t i = 0; i < b.fractions.size(); i++) {
+                v.fractions.push_back(b.fractions[i]);
+            }
+        }
         return v;
  }
-    CRat CRat::operator-(const CRat & b) {
-        int  n;
+CRat CRat::operator-(const CRat & b) {
+
+        size_t n = std::min(fractions.size(), b.fractions.size());
         CRat v;
-        n = fractions.size();
-        for (int i = 0; i < n; i++) {
-            if ((fractions[i].first != 0) && (b.fractions[i].first != 0)) {
-                v.fractions[i].first = fractions[i].first * b.fractions[i].second - fractions[i].second * b.fractions[i].first;
-                v.fractions[i].second = b.fractions[i].second * fractions[i].second;
-                int a = v.fractions[i].first;
-                int b = v.fractions[i].second;
+        for (size_t i = 0; i < n; i++) {
+            std::pair <int, unsigned int> p = std::make_pair(fractions[i].first * b.fractions[i].second - fractions[i].second * b.fractions[i].first, b.fractions[i].second * fractions[i].second);
+            v.fractions.push_back(p);
+        }
+        if (n < fractions.size())
+        {
+            for (size_t i = 0; i < fractions.size(); i++) {
+                v.fractions.push_back(fractions[i]);
             }
         }
-
+        if (n < fractions.size())
+        {
+            for (size_t i = 0; i < b.fractions.size(); i++) {
+                v.fractions.push_back(b.fractions[i]);
+            }
+        }
         return v;
-    }
-    CRat CRat::operator*(int k) {
-        int  n;
-        CRat v;
-        n = fractions.size();
-        for (int i = 0; i < n; i++) {
-            v.fractions[i].first = k * fractions[i].first;
-            v.fractions[i].second = fractions[i].second;
-        }
-        v.reduce();
-    return v;
-    }
-    CRat& CRat::reduce() {
-        int n = fractions.size();
-        for (int i = 0; i < n; i++) {
-
-            int a = fractions[i].first;
-            int b = fractions[i].second;
-            a = abs(a); b = abs(b);
-            while (a != 0 and b != 0) {
-                if (a > b) {
-                    a = a % b;
-                }
-                else {
-                    b = b % a;
-                }
+}
+CRat CRat::operator*(int k) {
+CRat v;
+size_t n = fractions.size();
+for (size_t i = 0; i < n; i++) {
+    v.fractions.push_back(std::make_pair(fractions[i].first * k , fractions[i].second));
+}
+v.reduce();
+return v;
             }
-            fractions[i].first = fractions[i].first / (a + b);
-            fractions[i].second = fractions[i].second / (a + b);
-        }
-        
-        return *this;
-    }
-     CRat& CRat :: operator=(const CRat& b) {
-         int n = fractions.size();
-         for (int i = 0; i < n; i++) {
-             fractions[i].first = b.fractions[i].first;
-             fractions[i].second = b.fractions[i].second;
-         }
-         return *this;
-     }
-     int CRat ::check(const CRat& b,const CRat& a) {
-         reduce();
-         int n = fractions.size();
-         int x = 0;
-         for (int i = 0; i < n; i++) {
-             if ((a.fractions[i].first == b.fractions[i].first) && (a.fractions[i].second == b.fractions[i].second)) {
-                 x += 1;
-             }
-         }
-         return x;
-     }
+CRat& CRat::reduce() {
+size_t n = fractions.size();
+ for (size_t i = 0; i < n; i++) {
+
+ unsigned int a = abs(fractions[i].first);
+ unsigned int b = fractions[i].second;
+  while (a != 0 and b != 0) {
+     if (a > b) {
+     a = a % b;
+  }
+  else {
+    b = b % a;
+  }
+ }
+ fractions[i].first = fractions[i].first / (a + b);
+ fractions[i].second = fractions[i].second / (a + b);
+ }
+
+return *this;
+}
+CRat& CRat :: operator=(const CRat & b) {
+int n = fractions.size();
+for (int i = 0; i < n; i++) {
+ fractions[i].first = b.fractions[i].first;
+ fractions[i].second = b.fractions[i].second;
+}
+return *this;
+}
+ bool CRat :: operator!=(const CRat & b) {
+  size_t n = fractions.size();
+  if (n == b.fractions.size()) {
+   return false;
+  }
+ int k = 0;
+ for (size_t i = 0; i < n; i++) {
+   if (fractions[i].first * b.fractions[i].second == fractions[i].second * b.fractions[i].first) {
+    k += 1;
+   }
+ }
+if (k == n)
+{
+ return false;
+}
+return true;
+ }
